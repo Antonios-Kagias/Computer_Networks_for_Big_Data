dacf96c676afb0f867133ff34d764909b3dd3d56
Antonios-Kagias <115150861+Antonios-Kagias@users.noreply.github.com>
2023-09-27 12:06:28 +0300

    Add files via upload

---
+++ 2 - Flow_Migration/flows/delete_new_flow2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo ovs-ofctl del-flows s6
+sudo ovs-ofctl del-flows s8
+sudo ovs-ofctl del-flows s9
+sudo ovs-ofctl del-flows s11
+sudo ovs-ofctl del-flows s10
+sudo ovs-ofctl del-flows s7
+sudo ovs-ofctl del-flows s5
+sudo ovs-ofctl del-flows s4
+sudo ovs-ofctl del-flows s2
---
+++ 2 - Flow_Migration/flows/flow3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo ovs-ofctl add-flow s6 dl_src=00:00:00:00:00:06,dl_dst=00:00:00:00:00:07,actions=output:4
+sudo ovs-ofctl add-flow s6 dl_src=00:00:00:00:00:07,dl_dst=00:00:00:00:00:06,actions=output:3
+sudo ovs-ofctl add-flow s8 dl_src=00:00:00:00:00:06,dl_dst=00:00:00:00:00:07,actions=output:2
+sudo ovs-ofctl add-flow s8 dl_src=00:00:00:00:00:07,dl_dst=00:00:00:00:00:06,actions=output:1
+sudo ovs-ofctl add-flow s7 dl_src=00:00:00:00:00:06,dl_dst=00:00:00:00:00:07,actions=output:2
+sudo ovs-ofctl add-flow s7 dl_src=00:00:00:00:00:07,dl_dst=00:00:00:00:00:06,actions=output:3
