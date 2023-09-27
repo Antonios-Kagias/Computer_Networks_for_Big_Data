958bcf144faf4fef545cf912fbf3db37af2a47ed
Antonios-Kagias <115150861+Antonios-Kagias@users.noreply.github.com>
2023-09-27 12:43:53 +0300

    Delete 2 - Flow_Migration directory

--- 2 - Flow_Migration/flows/delete_new_flow1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo ovs-ofctl del-flows s11
-sudo ovs-ofctl del-flows s9
-sudo ovs-ofctl del-flows s8
-sudo ovs-ofctl del-flows s6
-sudo ovs-ofctl del-flows s3
-sudo ovs-ofctl del-flows s2
-sudo ovs-ofctl del-flows s4
--- 2 - Flow_Migration/flows/flow2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo ovs-ofctl add-flow s2 dl_src=00:00:00:00:00:02,dl_dst=00:00:00:00:00:06,actions=output:3
-sudo ovs-ofctl add-flow s2 dl_src=00:00:00:00:00:06,dl_dst=00:00:00:00:00:02,actions=output:2
-sudo ovs-ofctl add-flow s3 dl_src=00:00:00:00:00:02,dl_dst=00:00:00:00:00:06,actions=output:4
-sudo ovs-ofctl add-flow s3 dl_src=00:00:00:00:00:06,dl_dst=00:00:00:00:00:02,actions=output:2
-sudo ovs-ofctl add-flow s6 dl_src=00:00:00:00:00:02,dl_dst=00:00:00:00:00:06,actions=output:3
-sudo ovs-ofctl add-flow s6 dl_src=00:00:00:00:00:06,dl_dst=00:00:00:00:00:02,actions=output:1
