f02467014056a25b32b28341578e7aa704a97ed4
Antonios-Kagias <115150861+Antonios-Kagias@users.noreply.github.com>
2023-09-27 12:42:30 +0300

    Add files via upload

---
+++ 2 - Flow Migration/flows/delete_new_flow3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo ovs-ofctl del-flows s6
+sudo ovs-ofctl del-flows s8
+sudo ovs-ofctl del-flows s9
+sudo ovs-ofctl del-flows s11
+sudo ovs-ofctl del-flows s10
+sudo ovs-ofctl del-flows s7
